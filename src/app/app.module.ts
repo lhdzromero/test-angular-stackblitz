5ed379fb42ee055bcfc2633a7ce40b775a734992
lhdzromero <lhdzromero@gmail.com>
2019-04-24 13:09:37 -0500

    curso angular 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { FrutaComponent } from './fruta/fruta.component';
+import { EmpleadoComponent } from './empleados/empleado.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, 
+                  HelloComponent,
+                  FrutaComponent,
+                  EmpleadoComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
