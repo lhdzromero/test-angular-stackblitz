cca6357cf6dfd4916fa678c293498f4b4908c607
lhdzromero <lhdzromero@gmail.com>
2020-01-16 14:00:31 -0600

    2 files modified

--- src/app/empleados/empleado.component.ts
+++ src/app/empleados/empleado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado';
 
 @Component({
   selector: 'empleado-tag',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 
 export class EmpleadoComponent{
   public titulo = 'Componente empleado';
+  public empleado:Empleado;
+  public trabajadores:Array<Empleado>;
+
+  constructor(){
+    this.empleado = new Empleado('Veronica', 45, 'Cocinera', true);
+    this.trabajadores = [
+      new Empleado('Luis', 39, 'Programador', false),
+      new Empleado('Pedro', 42, 'Analista', true),
+      new Empleado('Yazmin', 28, 'Tester', true),
+      new Empleado('Paola', 20, 'Becaria', true)
+    ];
+  }
+
+  ngOnInit(){
+    //this.empleado = new Empleado('Veronica', 45, 'Cocinera', true);
+    console.log(this.trabajadores);
+  }
+
+
 }
